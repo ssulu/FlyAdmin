c5ab16186b1afa381899d67e7ef56b060915a78b
skiy <dev@skiy.net>
2019-02-21 14:15:02 +0800

    reflush tab cache

--- js/index.js
+++ js/index.js
@@ -14,6 +14,9 @@ layui.config({
         $ = layui.jquery,
         layer = layui.layer,
         navbar = layui.navbar();
+
+    var menu = {};
+
     tab = layui.tab({
         elem: '.admin-nav-card' //设置选项卡容器
         ,
@@ -23,14 +26,13 @@ layui.config({
         //},
         contextMenu: true,
         onSwitch: function (data) {
-            console.log(data.id); //当前Tab的Id
-            console.log(data.index); //得到当前Tab的所在下标
-            console.log(data.elem); //得到当前的Tab大容器
+            // console.log(data.id); //当前Tab的Id
+            // console.log(data.index); //得到当前Tab的所在下标
+            // console.log(data.elem); //得到当前的Tab大容器
 
-            console.log(tab.getCurrentTabId())
+            // console.log(tab.getCurrentTabId())
         },
         closeBefore: function (obj) { //tab 关闭之前触发的事件
-            console.log(obj);
             //obj.title  -- 标题
             //obj.url    -- 链接地址
             //obj.id     -- id
@@ -39,20 +41,49 @@ layui.config({
                 layer.confirm('确定要关闭' + obj.title + '吗?', { icon: 3, title: '系统提示' }, function (index) {
                     //因为confirm是非阻塞的，所以这里关闭当前tab需要调用一下deleteTab方法
                     tab.deleteTab(obj.tabId);
+                    closeTab(obj.tabId);
+
                     layer.close(index);
                 });
                 //返回true会直接关闭当前tab
                 return false;
-            }else if(obj.title==='表单'){
+            } else if (obj.title === '表单') {
                 layer.confirm('未保存的数据可能会丢失哦，确定要关闭吗?', { icon: 3, title: '系统提示' }, function (index) {
                     tab.deleteTab(obj.tabId);
+                    closeTab(obj.tabId);
+
                     layer.close(index);
                 });
                 return false;
             }
+
+            closeTab(obj.tabId);
             return true;
         }
     });
+
+    //缓存标签
+    var menuStr = sessionStorage.getItem('menu');
+    if (menuStr != null) {
+        var menuObj = JSON.parse(menuStr);
+        menu = {};
+        for (const key in menuObj) {
+            addTab(menuObj[key]);
+        }
+    }
+
+    function closeTab(tabId) {
+        delete menu[tabId];
+        sessionStorage.setItem('menu', JSON.stringify(menu));
+    }
+
+    function addTab(info) {
+        tab.tabAdd(info);
+        var currentTabId = tab.getCurrentTabId();
+        menu[currentTabId] = info;
+        sessionStorage.setItem('menu', JSON.stringify(menu));
+    }
+
     //iframe自适应
     $(window).on('resize', function () {
         var $content = $('.admin-nav-card .layui-tab-content');
@@ -67,16 +98,17 @@ layui.config({
         spreadOne: true,
         elem: '#admin-navbar-side',
         cached: true,
-        data: navs
-		/*cached:true,
+        data: navs/*,
+		cached:true,
 		url: 'datas/nav.json'*/
     });
     //渲染navbar
     navbar.render();
     //监听点击事件
     navbar.on('click(side)', function (data) {
-        tab.tabAdd(data.field);
+        addTab(data.field);
     });
+
     //清除缓存
     $('#clearCached').on('click', function () {
         navbar.cleanCached();
